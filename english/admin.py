2ebd113381e6c85475ad6986e8390f8679a35d3b
nechtobolshee <ml5426883@gmail.com>
2022-06-16 01:27:51 +0300

    19718: created schedule

--- english/admin.py
+++ english/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from english.models import Class
+import english.models
+from english.models import Class, Schedule
 
 
 @admin.register(Class)
@@ -10,3 +11,15 @@ class AdminClass(admin.ModelAdmin):
     @staticmethod
     def number_of_students(obj):
         return obj.students.all().count()
+
+
+@admin.register(Schedule)
+class AdminSchedule(admin.ModelAdmin):
+    list_display = ("class_id", "full_selected_days_name", "start_time", "end_time")
+
+    @admin.display(description="Days")
+    def full_selected_days_name(self, obj):
+        all_days = english.models.Schedule.week_days
+        selected_days = obj.days
+        fullname_days = [item[1] for item in all_days if item[0] in selected_days]
+        return fullname_days
--- english/models.py
+++ english/models.py
@@ -1,4 +1,7 @@
+from django import forms
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+
 from users.models import User
 
 
@@ -9,3 +12,46 @@ class Class(models.Model):
 
     class Meta:
         verbose_name_plural = "Class"
+
+    def __str__(self):
+        return self.name
+
+
+class ChoiceArrayField(ArrayField):
+    def formfield(self, **kwargs):
+        defaults = {
+            "form_class": forms.MultipleChoiceField,
+            "choices": self.base_field.choices,
+        }
+        defaults.update(kwargs)
+        return super(ArrayField, self).formfield(**defaults)
+
+
+class Schedule(models.Model):
+    week_days = [
+        ("1", "Monday"),
+        ("2", "Tuesday"),
+        ("3", "Wednesday"),
+        ("4", "Thursday"),
+        ("5", "Friday"),
+        ("6", "Saturday"),
+        ("7", "Sunday"),
+    ]
+
+    class_id = models.ForeignKey(
+        Class,
+        on_delete=models.CASCADE,
+        verbose_name="Class name",
+        related_name="class_name",
+    )
+    days = ChoiceArrayField(
+        base_field=models.CharField(max_length=10, choices=week_days),
+        default=list,
+        size=5,
+        blank=True,
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = "Schedule"
