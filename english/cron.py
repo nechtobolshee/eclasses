cc13d1a2fd031609805ae578379391799340d13d
nechtobolshee <ml5426883@gmail.com>
2022-09-15 02:12:56 +0300

    20379: fixed crontab

--- english/cron.py
+++ english/cron.py
@@ -7,15 +7,15 @@ logger = logging.getLogger('django')
 
 class ChangeLessonsStatusJob:
     Lessons.objects.exclude(_status__in=[Lessons.CANCELED, Lessons.COMING_SOON]).filter(
-        time_start__gt=datetime.now()
+        start_time__gt=datetime.now()
     ).update(_status=Lessons.COMING_SOON)
 
     Lessons.objects.exclude(_status__in=[Lessons.CANCELED, Lessons.IN_PROGRESS]).filter(
-        time_start__lte=datetime.now(), time_end__gt=datetime.now()
+        start_time__lte=datetime.now(), end_time__gt=datetime.now()
     ).update(_status=Lessons.IN_PROGRESS)
 
     Lessons.objects.exclude(_status__in=[Lessons.CANCELED, Lessons.DONE]).filter(
-        time_end__lte=datetime.now()
+        end_time__lte=datetime.now()
     ).update(_status=Lessons.DONE)
 
 
